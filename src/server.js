8fedaab07d5c9c6bf4423ab7141811e3d5e289bf
idanpostman <idanpostman@gmail.com>
2025-03-04 16:19:19 +0700

    Inisiasi Project

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express, { json } from "express";
+import { config } from "dotenv";
+// import cors from "cors";
+// import routerApi from "./routes/index.js";
+
+config();
+const app = express();
+const port = process.env.PORT;
+
+// app.use(cors);
+app.use(json());
+
+app.get("/", (req, res) => {
+  res.send("Tahu Suzuka Punya Bapak Ade");
+});
+
+// routerApi(app);
+
+// Menjalankan server
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
